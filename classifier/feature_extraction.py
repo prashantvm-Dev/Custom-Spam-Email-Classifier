1f4dacfcc215bb4b6155ff8ea7d3f2e602e2b091
Prashant Kumar <prashantvishwakarma5083@gmail.com>
2026-08-24 00:08:56 +0530

    Delete classifier/feature_extraction.py

--- classifier/feature_extraction.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-
-def create_vectorizer():
-    return TfidfVectorizer()
-
-
-def extract_features(vectorizer, text):
-    return vectorizer.transform(text)
